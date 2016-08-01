e657acaa4d0a95344310ec2255a2823fc19dd352
ben.wang <ben.wangdev@gmail.com>
2016-08-02 00:00:09 +0800

    change pom add junit

---
+++ src/test/java/com/qunar/ben/leetcode/SearchForARangeTest.java
@@ -0,0 +1,45 @@
+package test.com.qunar.ben.leetcode; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* SearchForARange Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>°ËÔÂ 1, 2016</pre> 
+* @version 1.0 
+*/ 
+public class SearchForARangeTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: searchRange(int[] nums, int target) 
+* 
+*/ 
+@Test
+public void testSearchRange() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: main(String[] args) 
+* 
+*/ 
+@Test
+public void testMain() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
