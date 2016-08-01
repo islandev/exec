5b48287eb3e3cdd0ef673957792c7e81e3ac28ca
Ben <ben@qitmac000487deMacBook-Pro.local>
2016-08-01 20:50:01 +0800

    add array

--- src/main/java/com/qunar/ben/leetcode/SearchForARange.java
+++ src/main/java/com/qunar/ben/leetcode/SearchForARange.java
@@ -1,5 +1,10 @@
 package com.qunar.ben.leetcode;
 
+import com.sun.tools.javac.util.ArrayUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 
 /**
@@ -8,17 +13,34 @@ package com.qunar.ben.leetcode;
  */
 public class SearchForARange {
 
-    public  static  int[] searchRange(int[] nums, int target) {
-        int n = nums.length;
-        int[] range ={};
-        for(int i=n/2 ; i>=0;i<=n)
-        return null;
+    public  static List<Integer> searchRange(int[] nums, int target) {
+        int  low = 0;
+        int high = nums.length -1 ;
+        ArrayList<Integer> indexVal= new ArrayList<Integer>();
+        BinarySearchR(low,high,nums,target);
+
+        return  indexVal;
+
+    }
+    public  static  void BinarySearchR(int start, int end , int[] num, int target){
+        while(start<end){
+            int mid = (start +end)/2;
+            if(target >num[mid]) BinarySearchR(mid,end,num,target);
+            if(target<num[mid])  BinarySearchR(start,mid,num,target);
+            if(target == num[mid]){
+                System.out.println(mid);
+             if(mid+1<end)   BinarySearchR(mid+1,end,num,target);
+             if(mid-1>start)   BinarySearchR(start,mid-1,num,target);
+            }
+
+        }
     }
 
     public static void main(String[] args){
-        int[] nums = {5, 7, 7, 8, 8, 10};
+        int[] nums = {5, 7, 7, 8,8, 8, 10};
         int target = 8 ;
-        int[] range = searchRange(nums,target);
+        List<Integer> range= searchRange(nums,target);
+
     }
 
 }
