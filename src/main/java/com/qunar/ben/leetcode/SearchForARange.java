a0bb8ca02bb7dc30b8a2ca981b7129c981706984
Ben <ben@qitmac000487deMacBook-Pro.local>
2016-08-01 20:06:22 +0800

    add file

---
+++ src/main/java/com/qunar/ben/leetcode/SearchForARange.java
@@ -0,0 +1,24 @@
+package com.qunar.ben.leetcode;
+
+
+
+/**
+ * Created by ben on 16/8/1.
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ */
+public class SearchForARange {
+
+    public  static  int[] searchRange(int[] nums, int target) {
+        int n = nums.length;
+        int[] range ={};
+        for(int i=n/2 ; i>=0;i<=n)
+        return null;
+    }
+
+    public static void main(String[] args){
+        int[] nums = {5, 7, 7, 8, 8, 10};
+        int target = 8 ;
+        int[] range = searchRange(nums,target);
+    }
+
+}
