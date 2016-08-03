8215a1dbed2037ba12017f4176eaf02a12572bf7
Ben <ben@qitmac000487deMacBook-Pro.local>
2016-08-03 19:55:20 +0800

    top k

---
+++ src/main/java/com/qunar/ben/leetcode/TopKFrequetEl.java
@@ -0,0 +1,14 @@
+package com.qunar.ben.leetcode;
+
+import java.util.List;
+
+/**
+ * Created by ben on 16/8/2.
+ * Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+ * You may assume k is always valid, 1 ≤ k ≤ number of unique elements.
+ */
+public class TopKFrequetEl {
+    public List<Integer> topKFrequent(int[] nums, int k){
+        
+    }
+}
