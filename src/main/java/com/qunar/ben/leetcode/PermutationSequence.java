c67ba68c6aae58debee944557c19070e6700b068
islandev <ben.wangdev@gmail.com>
2017-02-27 00:47:18 +0800

    add ps

---
+++ src/main/java/com/qunar/ben/leetcode/PermutationSequence.java
@@ -0,0 +1,18 @@
+package com.qunar.ben.leetcode;
+
+/**
+ * Created by Administrator on 2017/2/27.
+ */
+public class PermutationSequence {
+    public String getPermutation(int n, int k) {
+        StringBuilder sb = new StringBuilder();
+        for (int i=0;i<n;i++){
+
+        }
+        return  "";
+    }
+
+    public  int getVal(int n, int k){
+        return 0;
+    }
+}
