eb444626fc2589809c925553d12e545867394f3d
islandev <ben.wangdev@gmail.com>
2017-02-27 00:56:04 +0800

    'ps'

--- src/main/java/com/qunar/ben/leetcode/PermutationSequence.java
+++ src/main/java/com/qunar/ben/leetcode/PermutationSequence.java
@@ -1,5 +1,7 @@
 package com.qunar.ben.leetcode;
 
+import java.util.List;
+
 /**
  * Created by Administrator on 2017/2/27.
  */
@@ -12,7 +14,22 @@ public class PermutationSequence {
         return  "";
     }
 
-    public  int getVal(int n, int k){
+    public  int getVal(int n, int k, List<Integer> arr){
+        int index = k/fab(n-1);
+        int res = arr.get(index);
+        arr.remove(index);
         return 0;
+    }
+
+    public  int fab(int val){
+        if(val<0){
+            throw  new IllegalArgumentException();
+        }
+        int res =1;
+        for(int i=2;i<=val;i++){
+            res *=i;
+        }
+        return  res;
+
     }
 }
