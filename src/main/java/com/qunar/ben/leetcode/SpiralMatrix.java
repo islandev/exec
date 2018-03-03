5a442cfda42a8e3f1f4647302294ab3aff76b8d7
ben <ben.wangdev@gmail.com>
2018-03-03 22:59:54 +0800

    baseball

---
+++ src/main/java/com/qunar/ben/leetcode/SpiralMatrix.java
@@ -0,0 +1,19 @@
+/**
+ * Created by ben
+ * on 2018/3/3.
+ */
+
+package com.qunar.ben.leetcode;
+
+import java.util.List;
+
+public class SpiralMatrix {
+    public List<Integer> spiralOrder(int[][] matrix) {
+
+            return  null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/main/java/com/qunar/ben/sortalg/BucketSort.java
+++ src/main/java/com/qunar/ben/sortalg/BucketSort.java
@@ -2,20 +2,36 @@ package com.qunar.ben.sortalg;
 
 import java.util.ArrayList;
 import java.util.List;
-import com.google.common.collect.*;
 /**
  * Created by ben on 2016/11/2.
  */
 public class BucketSort {
 
-    public  static  void main(String args[]){
-
-        System.out.print(Lists.newArrayList(1));
+    public static void main(String[] args) {
+        int[] a = {4,6,2,5,6,8,7,9,1};
+        qucikSort(a,0,a.length-1);
+        for (int val: a
+                ) {
+            System.out.println(val);
+        }
     }
 
+    public static  void  qucikSort(int[] a, int low, int high){
+        int start=low;;
+        int end = high;
+        int p = a[low];
+        if(low<high){
+            while(low<high){
 
-    public  static  void  initBucket(int[] arr){
-
+                while(low<high&&a[high]>=p) high--;
+                a[low]=a[high];
+                while(low>high&&a[low]<=p) low++;
+                a[high]=a[low];
+            }
+            a[low]=p;
+            qucikSort(a,start,low-1);
+            qucikSort(a,low+1,end);
+        }
     }
 
 
