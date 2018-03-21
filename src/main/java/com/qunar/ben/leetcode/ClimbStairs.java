7eba2e26584deca410fd7eb02a7fd7ee95b8eb50
ben.wang <ben.wang@qunar.com>
2018-03-21 15:48:34 +0800

    add tree

--- src/main/java/com/qunar/ben/leetcode/ClimbStairs.java
+++ src/main/java/com/qunar/ben/leetcode/ClimbStairs.java
@@ -1,5 +1,9 @@
 package com.qunar.ben.leetcode;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -10,4 +14,112 @@ import org.slf4j.LoggerFactory;
 public class ClimbStairs {
 
   private static final Logger logger = LoggerFactory.getLogger(ClimbStairs.class);
+
+  public static int climbStairs(int n) {
+    if (n <= 2) {
+      return n;
+    }
+
+    int pre = 1;
+    int cur = 2;
+    int ans = 0;
+    int i = 3;
+    while (i <= n) {
+      ans = pre + cur;
+      pre = cur;
+      cur = ans;
+      i++;
+    }
+    return ans;
+  }
+
+  public ListNode deleteDuplicates(ListNode head) {
+    if (head == null) {
+      return head;
+    }
+    int curVal = head.val;
+    ListNode pre = head;
+    ListNode cur = head.next;
+    while (cur != null) {
+      if (cur.val == curVal) {
+        pre.next = cur.next;
+        cur = cur.next;
+      } else {
+        curVal = cur.val;
+        pre = cur;
+        cur = cur.next;
+      }
+    }
+    return head;
+  }
+
+  public static void main(String[] args) {
+    System.out.printf(String.valueOf(climbStairs(25)));
+  }
+
+
+  public void mergeAC(int A[], int m, int B[], int n) {
+    int i = m - 1, j = n - 1, k = m + n - 1;
+    while (i > -1 && j > -1) {
+      A[k--] = (A[i] > B[j]) ? A[i--] : B[j--];
+    }
+    while (j > -1) {
+      A[k--] = B[j--];
+    }
+  }
+
+  public boolean isSameTree(TreeNode p, TreeNode q) {
+    if (p == null && q == null) {
+      return true;
+    }
+    if (p == null || q == null || p.val != q.val) {
+      return false;
+    }
+
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+  }
+
+  public int maxDepth(TreeNode root) {
+    if (root == null) {
+      return 0;
+    }
+    int lDepth = maxDepth(root.left);
+    int rDepth = maxDepth(root.right);
+    return Math.max(lDepth, rDepth) + 1;
+  }
+
+  public List<List<Integer>> levelOrderBottom(TreeNode root) {
+    List<List<Integer>> res = new ArrayList<>();
+    if (root == null) {
+      return res;
+    }
+    Queue<TreeNode> queue = new ArrayDeque<>();
+    queue.offer(root);
+    while (queue.size() > 0) {
+      List<Integer> sub = new ArrayList<>();
+      int size = queue.size();
+      for (int i = 0; i < size; i++) {
+        TreeNode node = queue.poll();
+        sub.add(node.val);
+        if (node.left != null) {
+          queue.offer(node.left);
+        }
+        if (node.right != null) {
+          queue.offer(node.right);
+        }
+      }
+      res.add(0, sub);
+
+    }
+
+    return res;
+  }
+
+  public TreeNode sortedArrayToBST(int[] nums) {
+
+    return null;
+  }
+
+
 }
+
--- src/main/java/com/qunar/ben/leetcode/MaxSubArray.java
+++ src/main/java/com/qunar/ben/leetcode/MaxSubArray.java
@@ -55,9 +55,9 @@ public class MaxSubArray {
     return cur == 0 ? res : cur;
   }
 
-  public static  int[] plusOne(int[] digits) {
+  public static int[] plusOne(int[] digits) {
     int length = digits.length;
-    int[] resArray = new int[length +1];
+    int[] resArray = new int[length + 1];
     int carry = 1;
     for (int i = length - 1; i >= 0; i--) {
       int res = carry + digits[i];
@@ -72,15 +72,74 @@ public class MaxSubArray {
     int[] ans = new int[length];
 
     for (int i = 0; i < length; i++) {
-      ans[i] = resArray[i +1];
+      ans[i] = resArray[i + 1];
     }
     return ans;
   }
 
   public static void main(String[] args) {
-    int[] nums = {1};
-    int[] ints = plusOne(nums);
-    System.out.printf("hio");
+//    System.out.printf(addBinary("11", "1"));
+    System.out.printf(String.valueOf(myHSqrt(5)));
   }
 
+  public static String addBinary(String a, String b) {
+    StringBuilder sb = new StringBuilder();
+    int aLength = a.length();
+    int bLength = b.length();
+    int minLength = Math.min(aLength, bLength);
+    char zero = '0';
+    int carry = 0;
+
+    for (int i = 0; i < minLength; i++) {
+      int val = carry + a.charAt(aLength - i - 1) + b.charAt(bLength - i - 1) - zero * 2;
+      carry = val / 2;
+      sb.append(val % 2);
+    }
+    String longer = aLength > bLength ? a : b;
+    for (int i = minLength; i < longer.length(); i++) {
+      int val = carry + longer.charAt(longer.length() - i - 1) - zero;
+      carry = val / 2;
+      sb.append(val % 2);
+    }
+    if (carry != 0) {
+      sb.append(carry);
+    }
+    return sb.reverse().toString();
+  }
+
+  public static int mySqrt(int x) {
+    if (x == 1) {
+      return 1;
+    }
+    for (int i = 1; i <= x; i++) {
+      if (x / i < i) {
+        return i - 1;
+      }
+    }
+    return 0;
+  }
+
+
+  public static int myHSqrt(int x) {
+    if (x == 1) {
+      return 1;
+    }
+    int low = 0;
+    int high = x;
+    while (low < high) {
+      int mid = (low + high) / 2;
+      int val = x / mid;
+      if (val > mid) {
+        low = mid;
+      } else if (val < mid) {
+        high = mid;
+      } else {
+        return mid;
+      }
+      if (high - low == 1) {
+        return low;
+      }
+    }
+    return 0;
+  }
 }
